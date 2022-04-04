bfc9e1830d740dce845e213a93a98c49f75285a5
Amir R Baserinia <baserinia@gmail.com>
2022-04-03 19:36:09 -0700

    python3 updates

--- src/mnist_loader.py
+++ src/mnist_loader.py
@@ -1,23 +1,23 @@
 """
 mnist_loader
-~~~~~~~~~~~~
-
+-------------------------------------------------------------------------------
 A library to load the MNIST image data.  For details of the data
 structures that are returned, see the doc strings for ``load_data``
 and ``load_data_wrapper``.  In practice, ``load_data_wrapper`` is the
 function usually called by our neural network code.
 """
 
-#### Libraries
-# Standard library
-import cPickle
+# Libraries
+# Standard libraries
+import pickle
 import gzip
 
 # Third-party libraries
 import numpy as np
 
 def load_data():
-    """Return the MNIST data as a tuple containing the training data,
+    """
+    Return the MNIST data as a tuple containing the training data,
     the validation data, and the test data.
 
     The ``training_data`` is returned as a tuple with two entries.
@@ -39,9 +39,8 @@ def load_data():
     That's done in the wrapper function ``load_data_wrapper()``, see
     below.
     """
-    f = gzip.open('../data/mnist.pkl.gz', 'rb')
-    training_data, validation_data, test_data = cPickle.load(f)
-    f.close()
+    with gzip.open('../data/mnist.pkl.gz', 'rb') as f:
+        training_data, validation_data, test_data = pickle.load(f, encoding='bytes')
     return (training_data, validation_data, test_data)
 
 def load_data_wrapper():
@@ -65,21 +64,28 @@ def load_data_wrapper():
     the training data and the validation / test data.  These formats
     turn out to be the most convenient for use in our neural network
     code."""
+
     tr_d, va_d, te_d = load_data()
+
     training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
-    training_results = [vectorized_result(y) for y in tr_d[1]]
-    training_data = zip(training_inputs, training_results)
+    training_results = [vectorize_result(y) for y in tr_d[1]]
+    training_data = list(zip(training_inputs, training_results))
+
     validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
-    validation_data = zip(validation_inputs, va_d[1])
+    validation_data = list(zip(validation_inputs, va_d[1]))
+
     test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
-    test_data = zip(test_inputs, te_d[1])
+    test_data = list(zip(test_inputs, te_d[1]))
+
     return (training_data, validation_data, test_data)
 
-def vectorized_result(j):
-    """Return a 10-dimensional unit vector with a 1.0 in the jth
+def vectorize_result(j):
+    """
+    Return a 10-dimensional unit vector with a 1.0 in the jth
     position and zeroes elsewhere.  This is used to convert a digit
     (0...9) into a corresponding desired output from the neural
-    network."""
+    network.
+    """
     e = np.zeros((10, 1))
     e[j] = 1.0
     return e

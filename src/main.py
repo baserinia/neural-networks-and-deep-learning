433e632d528377e87f346644aeb2d318b00d1e63
Amir R Baserinia <baserinia@gmail.com>
2022-04-03 20:22:41 -0700

    line ending fixed

--- src/main.py
+++ src/main.py
@@ -2,12 +2,16 @@
 
 import mnist_loader
 import network
+import mnist_svm
 
-def main():
+def run_network():
     training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
     net = network.Network([784, 100, 10])
     net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
+
+def main():
+    run_network()
 
 if __name__ == "__main__":
     main()
--- src/mnist_svm.py
+++ src/mnist_svm.py
@@ -1,9 +1,4 @@
-"""
-mnist_svm
-~~~~~~~~~
-
-A classifier program for recognizing handwritten digits from the MNIST
-data set, using an SVM classifier."""
+#!/usr/bin/env python3  
 
 #### Libraries
 # My libraries
@@ -14,14 +9,16 @@ from sklearn import svm
 
 def svm_baseline():
     training_data, validation_data, test_data = mnist_loader.load_data()
+    print("Data loaded ...")
     # train
     clf = svm.SVC()
     clf.fit(training_data[0], training_data[1])
+    print("Training finished ...")
     # test
     predictions = [int(a) for a in clf.predict(test_data[0])]
     num_correct = sum(int(a == y) for a, y in zip(predictions, test_data[1]))
-    print "Baseline classifier using an SVM."
-    print "%s of %s values correct." % (num_correct, len(test_data[1]))
+    print("Baseline classifier using an SVM.")
+    print(f"{num_correct} of {len(test_data[1])} values correct.")
 
 if __name__ == "__main__":
     svm_baseline()
